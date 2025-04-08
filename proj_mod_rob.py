b0e2600cb013a2d2acdfa783f61a67f683d06d5a
Dan-project <calamia.dan@gmail.com>
2025-04-08 16:42:56 +0200

    class robot complete

--- proj_mod_rob.py
+++ proj_mod_rob.py
@@ -13,7 +13,18 @@ class Robot():
         self.Re=Re
         self.q = []
     
-        
+    def get_L1(self):
+        return self.L1
+    
+    def get_L2(self):
+        return self.L2
+    
+    def get_Rb(self):
+        return self.Rb
+    
+    def get_Re(self):
+        return self.LRe
+
     def traceRobot(self, alpha1, beta1, alpha2, beta2, alpha3, beta3) :
         self.q[0] = alpha1
         self.q[1] = beta1
@@ -145,10 +156,135 @@ class Robot():
 
         plt.show()
 
+    def MGI_analytique(eff):
+        # Variables globales
+        global L1, L2, Rb, Re
+
+        # Matrice de rotation et translation de l'effecteur
+        RotEff = np.array([[np.cos(eff[2]), -np.sin(eff[2])], [np.sin(eff[2]), np.cos(eff[2])]])
+        Transl = np.array([eff[0], eff[1]])
+        THEff = np.block([[RotEff, Transl.reshape(-1, 1)], [0, 0, 1]])
+
+        # Angles R_i par rapport à R_0
+        ang1 = [0, 2 * np.pi / 3, 4 * np.pi / 3]
+
+        # Angles des positions O_i et E_i
+        ang2 = [-np.pi / 2, np.pi / 6, 5 * np.pi / 6]
+
+        q = []
+
+        # Calcul pour chaque bras
+        for i in range(3):
+            # Matrice de rotation et translation de R_i par rapport à R_0
+            Rot = np.array([[np.cos(ang1[i]), -np.sin(ang1[i])], [np.sin(ang1[i]), np.cos(ang1[i])]])
+            TH = np.block([[Rot, np.array([Rb * np.cos(ang2[i]), Rb * np.sin(ang2[i])]).reshape(-1, 1)], [0, 0, 1]])
+
+            # Position des points E_i dans R_E
+            PEi_E = np.array([Re * np.cos(ang2[i]), Re * np.sin(ang2[i]), 1])
+
+            # Position des trois points E_i de l'effecteur dans R_0
+            PEi_0 = THEff @ PEi_E
+
+            # Position des points effecteur E_i dans les repères des robots R_i
+            PEi_i = np.linalg.inv(TH) @ PEi_0
+
+            # MGI 2R plan
+            x = PEi_i[0]
+            y = PEi_i[1]
+
+            aux = (x**2 + y**2 - L1**2 - L2**2) / (2 * L1 * L2)
+            if abs(aux) < 1:
+                beta = np.arccos(aux)  # Solution coude en haut
+            else:
+                beta = 0
+                print("Problème d'atteignabilité")
+
+            alpha = np.arctan2(y, x) - np.arctan2(L2 * np.sin(beta), L1 + L2 * np.cos(beta))
+
+            q.append([alpha, beta])
+
+        return np.array(q)
+
+    def solve_eq_NL(q, eff):
+        # Variables globales
+        global L1, L2, Rb, Re
+
+        # Extraction des valeurs de alpha et beta pour chaque bras
+        alpha = [q[0], q[2], q[4]]
+        beta = [q[1], q[3], q[5]]
+
+        # Angles R_i par rapport à R_0
+        ang1 = [0, 2 * np.pi / 3, 4 * np.pi / 3]
+
+        # Angles des positions O_i et E_i
+        ang2 = [-np.pi / 2, np.pi / 6, 5 * np.pi / 6]
+
+        # Matrice de rotation et translation de l'effecteur
+        RotEff = np.array([[np.cos(eff[2]), -np.sin(eff[2])], [np.sin(eff[2]), np.cos(eff[2])]])
+        Transl = np.array([eff[0], eff[1]])
+        THEff = np.block([[RotEff, Transl.reshape(-1, 1)], [0, 0, 1]])
+
+        F = []
+
+        for i in range(3):
+            # Position des points E_i dans R_E
+            PEi_E = np.array([Re * np.cos(ang2[i]), Re * np.sin(ang2[i]), 1])
+
+            # Position des trois points E_i de l'effecteur dans R_0
+            PEi_0 = THEff @ PEi_E
+
+            # Matrice de rotation et translation de R_i par rapport à R_0
+            Rot = np.array([[np.cos(ang1[i]), -np.sin(ang1[i])], [np.sin(ang1[i]), np.cos(ang1[i])]])
+            THRi_0 = np.block([[Rot, np.array([Rb * np.cos(ang2[i]), Rb * np.sin(ang2[i])]).reshape(-1, 1)], [0, 0, 1]])
+
+            # Points B_i, extrémités des bras en fonction de alpha_i et beta_i dans R_0
+            PBi = THRi_0 @ np.array([L1 * np.cos(alpha[i]) + L2 * np.cos(alpha[i] + beta[i]),
+                                    L1 * np.sin(alpha[i]) + L2 * np.sin(alpha[i] + beta[i]),
+                                    1])
+
+            # Les contraintes expriment que B_i doit être confondu avec E_i
+            # F(2*i-1) et F(2*i) doivent être égales à zéro (les coordonnées x et y)
+            F.append(PBi[0] - PEi_0[0])  # x
+            F.append(PBi[1] - PEi_0[1])  # y
+
+        return np.array(F)
+
+
+
 
 def main():
     robot= Robot(5,5,10,10)
     robot.traceRobot(pi/2,pi/2,pi/2,pi/2,pi/2,pi/2)
+
+    L1=0.10;  # long segment 1
+    L2=0.10;  # long segment 2
+    Rb=0.1322594;  # Rayon base
+    Re=0.07; # Rayon effecteur
+
+    pos_eff=[0.0, 0.0, 0]; # pose effecteur
+
+    # 1ière méthode : résolution de systèmes d'éq non-linéaires 
+    # solutions initiales des angles alpha beta des bras 1,2,3
+    q0= np.array([
+                [0], 
+                [pi/2], 
+                [0],
+                [pi/2 ],
+                [0],
+                [pi/2]])
+
+    
+    from scipy.optimize import fsolve
+    # Résoudre le système d'équations non linéaires
+    q = fsolve(lambda q: robot.solve_eq_NL(q, pos_eff), q0)
+
+
+    robot.traceRobot(q)
+
+    # 2ième méthode : résolution analytique du MGI 2R plan :renvoie alphi_i et beta_i
+    q=robot.MGI_analytique(pos_eff)
+    robot.traceRobot(q)
+
 
 if __name__=="__main__":
     main()
