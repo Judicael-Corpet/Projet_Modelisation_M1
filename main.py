af194bd72af44778f656febdbeeee65b58968b1d
Dan-project <calamia.dan@gmail.com>
2025-05-26 14:37:39 +0200

    valeurs Judi

--- main.py
+++ main.py
@@ -4,9 +4,9 @@ def main():
  
     # Données qu'on pourra modifier pour tester
     
-    L1=0.10;  # long segment 1
-    L2=0.10;  # long segment 2
-    Rb=0.1322594;  # Rayon base
+    L1=0.12;  # long segment 1
+    L2=0.12;  # long segment 2
+    Rb=0.13;  # Rayon base
     Re=0.07; # Rayon effecteur
     dimensionPlateau= 0.50 #  carré en cm
     pos_eff=[0.1, -0.02, 0.0]; # pose effecteur à choisir
